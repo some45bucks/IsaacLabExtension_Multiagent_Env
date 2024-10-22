c336e1b2d78aaf2a08083452fc0aae90a1cccaed
some45bucks <jhaightsnake@gmail.com>
2024-10-22 16:42:56 -0600

    renamed everything

--- scripts/list_envs.py
+++ scripts/list_envs.py
@@ -20,7 +20,7 @@ import gymnasium as gym
 from prettytable import PrettyTable
 
 # Import extensions to set up environment tasks
-import ext_template.tasks  # noqa: F401
+import Multiagent_Env.tasks  # noqa: F401
 
 
 def main():
--- scripts/rsl_rl/play.py
+++ scripts/rsl_rl/play.py
@@ -50,7 +50,7 @@ from omni.isaac.lab_tasks.utils.wrappers.rsl_rl import (
 )
 
 # Import extensions to set up environment tasks
-import ext_template.tasks  # noqa: F401
+import Multiagent_Env.tasks  # noqa: F401
 
 
 def main():
